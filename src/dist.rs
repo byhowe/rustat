1e2c4b9aae93997960e4afb45c5b605ba658bf4f
B. Howe <37745048+byhowe@users.noreply.github.com>
2023-01-09 13:43:49 +0300

    Implement unbounded ranges for cdfnorm

--- src/dist.rs
+++ src/dist.rs
@@ -6,20 +6,22 @@ use crate::normal::Normal;
 
 /// Calculates the area under a normal distribution curve within the given
 /// interval.
-pub fn cdf_normal<R: RangeBounds<f32>>(normal: Normal, interval: &R) -> f32
+pub fn cdf_normal<R: RangeBounds<f32>>(normal: Normal, interval: R) -> f32
 {
     let range = match (interval.start_bound(), interval.end_bound()) {
         (Bound::Included(s) | Bound::Excluded(s), Bound::Included(e) | Bound::Excluded(e)) => {
             *s..*e
         }
-        _ => unimplemented!(),
+        (Bound::Unbounded, Bound::Excluded(e) | Bound::Included(e)) => -15.0..*e,
+        (Bound::Excluded(s) | Bound::Included(s), Bound::Unbounded) => *s..15.0,
+        (Bound::Unbounded, Bound::Unbounded) => -15.0..15.0,
     };
-    calc::area(|x| normal.value(x), range)
+    calc::area(|x| normal.pdf(x), range)
 }
 
 /// Calculates the area under a standard normal distribution curve within the
 /// given interval.
-pub fn cdf_std_normal<R: RangeBounds<f32>>(interval: &R) -> f32
+pub fn cdf_std_normal<R: RangeBounds<f32>>(interval: R) -> f32
 {
     cdf_normal(Normal::standard(), interval)
 }
@@ -32,13 +34,16 @@ mod tests
     #[test]
     fn std_normal_table_values()
     {
-        let ranges = vec![0.0..1.0, 0.0..2.0, 0.0..3.0, -1.0..2.0];
-        let table = vec![0.34134, 0.47725, 0.49865, 0.34134 + 0.47725];
-        let values = ranges.iter().map(cdf_std_normal).collect::<Vec<_>>();
+        let values = vec![
+            (cdf_std_normal(0.0..1.0), 0.34134),
+            (cdf_std_normal(0.0..2.0), 0.47725),
+            (cdf_std_normal(0.0..3.0), 0.49865),
+            (cdf_std_normal(-1.0..2.0), 0.34134 + 0.47725),
+            (cdf_std_normal(..0.0), 0.5),
+        ];
         let diff = values
             .iter()
-            .enumerate()
-            .map(|(i, x)| (x - table[i]).abs())
+            .map(|(v, t)| (v - t).abs())
             .collect::<Vec<_>>();
         let acceptable = diff.iter().filter(|x| **x > 0.001).next().is_none();
         assert!(acceptable, "Values are not withing acceptable range")
